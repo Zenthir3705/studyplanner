2f2e1567a150c34fca290eeae9eea6497edce967
Zenthir3705 <hornung-matthias@web.de>
2018-10-19 16:35:46 +0200

    First Commit

---
+++ WebContent/Component.js
@@ -0,0 +1,71 @@
+sap.ui.define([
+	"sap/ui/core/UIComponent",
+	"sap/ui/model/json/JSONModel",
+	"hm/dhbw/cas/controller/PDFReader",
+	"hm/dhbw/cas/controller/CourseManager"
+], function(UIComponent, JSONModel, PDFReader, CourseManager) {
+	"use strict";
+	
+	return UIComponent.extend("hm.dhbw.cas.Component", {
+		
+		metadata : {
+			manifest : "json"
+		},
+		
+		/**
+		 * Event method for the init event of the component.
+		 * Initializes the device model, router and user role.
+		 * @public
+		 */
+		init : function() {
+			UIComponent.prototype.init.apply(this, arguments);
+			
+			this._showBusyDialog();
+			PDFReader.loadResources(this, {
+				success : function(aCourses) {
+					CourseManager.initialize(this, aCourses);
+
+					this.getRouter().initialize();
+					this._hideBusyDialog();
+				}.bind(this),
+				error : function() {
+					this.getRouter().initialize();
+				}.bind(this)
+			});
+		},
+		
+		/**
+		 * Shows the busy dialog.
+		 * @private
+		 */
+		_showBusyDialog : function() {
+			var that = this;
+			
+			if(!this._oDialog) {
+				this._oDialog = sap.ui.xmlfragment("hm.dhbw.cas.view.fragments.BusyDialog", this);
+				this._oDialog.setModel(new JSONModel({
+					title : this.getModel("i18n").getProperty("dialog_loading_title"),
+					title : this.getModel("i18n").getProperty("dialog_loading_description")
+				}));
+			}
+			
+			setTimeout( function() {
+				that._oDialog.open();
+			}, 0);
+		},
+		
+		/**
+		 * Hides the busy dialog.
+		 * @private
+		 */
+		_hideBusyDialog : function() {
+			var that = this;
+			
+			if(this._oDialog) {
+				setTimeout( function() {
+					that._oDialog.close();
+				}, 0);
+			}
+		}
+	});
+});
